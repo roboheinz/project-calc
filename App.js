7fe6640e03769e567b3a53cb4c0945646284eed3
Rapli18 <appymrafli@gmail.com>
2024-02-13 09:45:19 +0700

    install tailwind and add modal

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { View } from 'react-native';
 import Dashboard from './src/component/Dashboard';
 
-export default function App() {
-  return (
-    <View>
-      <Dashboard/>
-    </View>
-  );
-}
+const App = () => {
+  return <Dashboard />;
+};
+
+export default App;
--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,64 +1,115 @@
-import React from "react"
-import { SafeAreaView, TouchableOpacity, View, Text, Image, ScrollView } from "react-native";
+import React from 'react';
+import { SafeAreaView, TouchableOpacity, View, Text, Image, ScrollView, Modal, StyleSheet } from 'react-native';
 
 const Dashboard = () => {
+  const [openDatar, setOpenDatar] = React.useState(true);
+  const [openRuang, setOpenRuang] = React.useState(true);
+  // const [isModal, setIsModal] = React.useState(false);
+  const [modalVisible, setModalVisible] = React.useState(false);
 
-    const [openDatar, setOpenDatar] = React.useState(true);
-    const [openRuang, setOpenRuang] = React.useState(true);
-
-    const bangunData = [
+  const bangunData = [
+    {
+      datar: [
         {
-            "datar": [
-                {
-                    source: require(`../../assets/icon.png`),
-                    name: "Persegi"
-                },
-            ],
+          source: require(`../../assets/icon.png`),
+          name: 'Persegi',
+        },
+      ],
 
-            "ruang": [
-                "kubus",
-                "balok",
-                "prisma segitiga",
-                "kerucut",
-                "limas segitiga",
-                "limas segiempat",
-                "bola",
-                "tabung"
-            ]
-        }
-    ]
+      ruang: ['Kubus', 'Balok', 'Prisma Segitiga', 'Kerucut', 'Limas Segitiga', 'Limas Segiempat', 'Bola', 'Tabung'],
+    },
+  ];
 
-    return (
-        <SafeAreaView style={{justifyContent: 'center', alignItems: 'center', marginTop: 50}}>
-            <ScrollView>
-                <Text>Logo</Text>
+  return (
+    <SafeAreaView className="mt-14 items-center flex-1">
+      <Text>Logo</Text>
 
-                <TouchableOpacity onPress={() => setOpenDatar(!openDatar)} style={{marginTop: 50}}>
-                    <Text>Bangun Datar</Text>
-                </TouchableOpacity>
-                {
-                    bangunData[0].datar.map((items, index) => (
-                        <View key={index} style={openDatar ? {display: 'none'} : {display: 'block'}}>
-                            {/* <Image></Image> */}
-                            <Image source={items.source} style={{width: 100}}/>
-                            <Text>{items.name}</Text>
-                        </View>
-                    ))
-                }
+      <TouchableOpacity onPress={() => setOpenDatar(!openDatar)} className="mt-14">
+        <Text>Bangun Datar</Text>
+      </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => setOpenRuang(!openRuang)} style={{marginTop: 50}}>
-                    <Text>Bangun Ruang</Text>
-                </TouchableOpacity>
-                {
-                    bangunData[0].ruang.map((items, index) => (
-                        <View key={index} style={openRuang ? {display: 'none'} : {display: 'block'}}>
-                            <Text>{items}</Text>
-                        </View>
-                    )) 
-                }
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+      {/* POP UP */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View className="flex-1 justify-center mt-6">
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => setModalVisible(false)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      {/* POP UP */}
+
+      {bangunData[0].datar.map((items, index) => (
+        <TouchableOpacity key={index} className={openDatar ? 'hidden' : 'block bg-lime-300 p-4'} onPress={() => setModalVisible(true)}>
+          {/* <Image></Image> */}
+          {/* <Image source={items.source} /> */}
+          <Text>{items.name}</Text>
+        </TouchableOpacity>
+      ))}
+
+      <TouchableOpacity onPress={() => setOpenRuang(!openRuang)} className="mt-14">
+        <Text>Bangun Ruang</Text>
+      </TouchableOpacity>
+      {bangunData[0].ruang.map((items, index) => (
+        <View key={index} className={openRuang ? 'hidden' : 'block'}>
+          <Text>{items}</Text>
+        </View>
+      ))}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    // alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default Dashboard;
