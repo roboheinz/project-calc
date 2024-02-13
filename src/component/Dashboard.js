9d093144af36f02e91e11ae7e9a3e26e2ae50fcc
Rapli18 <appymrafli@gmail.com>
2024-02-13 13:01:49 +0700

    Add Popup

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,115 +1,119 @@
 import React from 'react';
-import { SafeAreaView, TouchableOpacity, View, Text, Image, ScrollView, Modal, StyleSheet } from 'react-native';
+import { SafeAreaView, TouchableOpacity, View, Text, Image, FlatList, ScrollView } from 'react-native';
+import PopupModal from './PopupModal';
 
 const Dashboard = () => {
-  const [openDatar, setOpenDatar] = React.useState(true);
+  const [openDatar, setOpenDatar] = React.useState(false);
   const [openRuang, setOpenRuang] = React.useState(true);
-  // const [isModal, setIsModal] = React.useState(false);
   const [modalVisible, setModalVisible] = React.useState(false);
+  const [activeItem, setActiveItem] = React.useState('');
 
   const bangunData = [
     {
       datar: [
         {
+          id: 1,
           source: require(`../../assets/icon.png`),
           name: 'Persegi',
         },
+        {
+          id: 2,
+          source: require(`../../assets/icon.png`),
+          name: 'AKOWDA',
+        },
+        {
+          id: 3,
+          source: require(`../../assets/icon.png`),
+          name: 'ACKNASC',
+        },
+        {
+          id: 4,
+          source: require(`../../assets/icon.png`),
+          name: 'askncasjo',
+        },
+        {
+          id: 5,
+          source: require(`../../assets/icon.png`),
+          name: 'askncasjo',
+        },
+        {
+          id: 6,
+          source: require(`../../assets/icon.png`),
+          name: 'askncasjo',
+        },
+        {
+          id: 7,
+          source: require(`../../assets/icon.png`),
+          name: 'askncasjo',
+        },
       ],
 
-      ruang: ['Kubus', 'Balok', 'Prisma Segitiga', 'Kerucut', 'Limas Segitiga', 'Limas Segiempat', 'Bola', 'Tabung'],
+      ruang: [
+        {
+          id: 1,
+          source: require(`../../assets/icon.png`),
+          name: 'afsasdas',
+        },
+        {
+          id: 2,
+          source: require(`../../assets/icon.png`),
+          name: 'afsasdas',
+        },
+        {
+          id: 3,
+          source: require(`../../assets/icon.png`),
+          name: 'afsasdas',
+        },
+      ],
     },
   ];
 
+  const touchablePress = (modal, items) => {
+    setModalVisible(modal);
+    setActiveItem(items);
+  };
+
   return (
-    <SafeAreaView className="mt-14 items-center flex-1">
-      <Text>Logo</Text>
+    <SafeAreaView className="flex-1">
+      <ScrollView className="min-w-full mt-10" contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}>
+        <Text>Logo</Text>
 
-      <TouchableOpacity onPress={() => setOpenDatar(!openDatar)} className="mt-14">
-        <Text>Bangun Datar</Text>
-      </TouchableOpacity>
-
-      {/* POP UP */}
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View className="flex-1 justify-center mt-6">
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => setModalVisible(false)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-      {/* POP UP */}
-
-      {bangunData[0].datar.map((items, index) => (
-        <TouchableOpacity key={index} className={openDatar ? 'hidden' : 'block bg-lime-300 p-4'} onPress={() => setModalVisible(true)}>
-          {/* <Image></Image> */}
-          {/* <Image source={items.source} /> */}
-          <Text>{items.name}</Text>
+        <TouchableOpacity onPress={() => setOpenDatar(!openDatar)} className="mt-14">
+          <Text>Bangun Datar</Text>
         </TouchableOpacity>
-      ))}
 
-      <TouchableOpacity onPress={() => setOpenRuang(!openRuang)} className="mt-14">
-        <Text>Bangun Ruang</Text>
-      </TouchableOpacity>
-      {bangunData[0].ruang.map((items, index) => (
-        <View key={index} className={openRuang ? 'hidden' : 'block'}>
-          <Text>{items}</Text>
-        </View>
-      ))}
+        <FlatList
+          data={bangunData[0].datar}
+          renderItem={({ item }) => (
+            <TouchableOpacity className={openDatar ? 'block bg-lime-300 p-4 mb-4' : 'hidden'} onPress={() => touchablePress(true, item)}>
+              {/* <Image alt={item.name} source={item.source} className="block h-5 max-w-full" /> */}
+              <Image alt={item.name} className="block h-5 max-w-full" />
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+          className={openDatar ? 'block min-w-full bg-red-700 max-h-60 flex-grow' : 'hidden'}
+        />
+
+        <PopupModal modalVisible={modalVisible} setModalVisible={setModalVisible} touchablePress={touchablePress} activeItem={activeItem} />
+
+        <TouchableOpacity onPress={() => setOpenRuang(!openRuang)} className="mt-14">
+          <Text>Bangun Ruang</Text>
+        </TouchableOpacity>
+
+        <FlatList
+          data={bangunData[0].ruang}
+          renderItem={({ item }) => (
+            <TouchableOpacity className={openRuang ? 'block bg-lime-300 p-4 mb-4' : 'hidden'} onPress={() => touchablePress(true, item)}>
+              {/* <Image alt={item.name} source={item.source} className="block h-5 max-w-full" /> */}
+              <Image alt={item.name} className="block h-5 max-w-full" />
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+          className={openRuang ? 'block min-w-full bg-red-700 max-h-60 flex-grow' : 'hidden'}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-});
 
 export default Dashboard;
---
+++ src/component/PopupModal.js
@@ -0,0 +1,66 @@
+import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const PopupModal = ({ modalVisible, setModalVisible, activeItem, touchablePress }) => {
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View className="flex-1 justify-center mt-6 bg-[#303030]">
+          <View style={styles.modalView}>
+            <Image alt="Persegi" width={50} height={50} className="bg-red-500" />
+            <Text style={styles.modalText}>{activeItem?.name}</Text>
+            <TouchableOpacity style={[styles.button, styles.buttonClose]} onPress={() => touchablePress(false, '')}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
+export default PopupModal;
